05db8458187418c4aa1d0f5a936c58d4249120a6
nordine-yzd <nordine-elyazidi@hotmail.fr>
2022-03-22 16:46:09 +0100

    creation database.ts

---
+++ test.js
@@ -0,0 +1,17 @@
+// import { MongoClient } from "mongodb";
+
+// const databaseUrl = process.env.MONGODB_DATABASE_URL || "";
+
+// const client = new MongoClient(databaseUrl);
+
+// const frida = {
+//   firstName: "Frida",
+//   lastName: "Ewlines",
+// };
+// client.connect().then((client) => {
+//   const db = client.db();
+//   // code here
+//   db.collection("users")
+//     .insertOne(frida)
+//     .then(() => client.close());
+// });
